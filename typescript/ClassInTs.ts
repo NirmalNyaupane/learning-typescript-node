6d0f1a47209586b71fc19cde1968f9f9f9d79654
Nirmal Neupane <neupanenirmal417@gmail.com>
2023-12-14 18:07:17 +0545

    Generics question added

--- typescript/ClassInTs.ts
+++ typescript/ClassInTs.ts
@@ -1,13 +1,13 @@
 class Person {
   name: string;
   email: string;
-  createdAt:Date;
+  createdAt: Date;
 
   //constructor is called when instance of class made
   constructor(name: string, email: string) {
     this.name = name;
     this.email = email;
-    this.createdAt = new Date()
+    this.createdAt = new Date();
   }
 
   getUserData() {
@@ -19,21 +19,40 @@ class Person {
   }
 }
 
-const person = new Person("Nirmal Neuapne","neupanenirmal");
+const person = new Person("Nirmal Neuapne", "neupanenirmal");
 console.log(person);
 
+/**
+ * Question:
+ * Image a JavaScript function that takes two parameters:
+ * an object and an array of keys. The function will return a new object based on the original one, but with only
+ * the keys you want:
+ */
 
+const pickUpKeys = <T extends { [key: string]: any }, K extends keyof T>(
+  obj: T,
+  keys: K[]
+) => {
+  let restult = {} as Pick<T, K>;
+
+  keys.map((key) => {
+    if (key in obj) {
+      restult[key] = obj[key];
+    }
+  });
+  return restult;
+};
 
 //getter and setter class
-class GetAndSet{
-    private number:number=0;
+class GetAndSet {
+  private number: number = 0;
 
-    setNumber(number:number){
-        this.number=number;
-    }
-    getNumber(){
-        return this.number
-    }
+  setNumber(number: number) {
+    this.number = number;
+  }
+  getNumber() {
+    return this.number;
+  }
 }
 
 const number = new GetAndSet();
